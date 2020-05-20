e9d39f0fe1300ccd4858314552c5289fdd6b6615
Juan Tarquino <jptarqu@gmail.com>
2020-05-19 21:45:37 -0400

    Initial commit, xaml runs

---
+++ src/CoreWfSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Activities;
+using System.Activities.XamlIntegration;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+
+namespace CoreWfSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xamlString = File.ReadAllText("sampleXaml.xml");
+            var activity = ActivityXamlServices.Load(new StringReader(xamlString), new ActivityXamlServicesSettings { CompileExpressions = true });
+            WorkflowInvoker.Invoke(activity, new Dictionary<string, Object>());
+        }
+    }
+}
---
+++ src/WorkflowForCorePlayground/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Activities
+Imports System.Activities.Statements
+Imports System.Diagnostics
+Imports System.Linq
+
+Module Module1
+
+    Sub Main()
+        Dim workflow1 = New Workflow1()
+        WorkflowInvoker.Invoke(workflow1)
+    End Sub
+
+End Module
